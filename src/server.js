0d403e5c9918bea9ff4f8e98292132cc5c32f04f
monkeyhurray <96379015+monkeyhurray@users.noreply.github.com>
2022-02-14 22:53:44 +0900

    newOne

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
-import express from "express";
+import express, { request } from "express";
 
 const PORT = 4000;
 
 const app = express();
+
+const handleHome = (req, res) => {
+  return res.send("I still love you");
+};
+
+app.get("/", handleHome);
+//application에게 home으로 gety request를 보낸다면 반응하는 callback을 만듬
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
