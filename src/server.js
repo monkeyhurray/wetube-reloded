a593de1dc162468914186897615ba172dd3f1ee6
monkeyhurray <96379015+monkeyhurray@users.noreply.github.com>
2022-02-16 23:26:50 +0900

    studyExpress

--- src/server.js
+++ src/server.js
@@ -4,11 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-  return res.send("I still love you");
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is going to:${req.url}`);
+  next();
 };
 
-app.get("/", handleHome);
+const handleHome = (req, res, next) => {
+  return res.send("I love middlewares");
+};
+
+app.get("/", gossipMiddleware, handleHome);
 //application에게 home으로 gety request를 보낸다면 반응하는 callback을 만듬
 
 const handleListening = () =>
