bfda2d9baaf00afcbd1c3d645a9e006485b1e2d7
monkeyhurray <96379015+monkeyhurray@users.noreply.github.com>
2022-02-24 22:12:16 +0900

    studyRouter

---
+++ README.md
@@ -0,0 +1,15 @@
+# Wetube Reloaded
+
+/ -> Home
+
+/join -> Join
+/login -> Login
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/viedos/comments -> Comment on a video
+/videos/comments/delete -> Delete A Comment of a Video
--- src/server.js
+++ src/server.js
@@ -1,35 +1,39 @@
-import express, { request } from "express";
-import { handle } from "express/lib/application";
+import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
+//middleware는 위에 존재해야함
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
 
-const logger = (req, res, next) => {
-  console.log(`${req.method}${req.url}`);
-  next();
-};
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, tou may continue");
-  next();
-};
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
 
-const handleHome = (req, res) => {
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const home = (req, res) => {
   return res.send("I love middlewares");
 };
 
-const handleProtected = (req, res) => {
+const login = (req, res) => {
   return res.send("Welcome to the private lounge.");
 };
-app.use(logger, privateMiddleware);
-//middleware는 위에 존재해야함
-app.get("/", handleHome);
+
+app.get("/", home);
 //application에게 home으로 gety request를 보낸다면 반응하는 callback을 만듬
-app.get("/protexted", handleProtected);
+app.get("/login", login);
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 
