e9f47aa3fbfa56833671e9ab006dfa9d021c6876
monkeyhurray <96379015+monkeyhurray@users.noreply.github.com>
2022-02-25 22:10:04 +0900

    study

---
+++ src/routers/userRouter.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const userRouter = express.Router();
+
+userRouter.get("/edit", handleEdit);
+userRouter.get("/delete", handleDelete);
+export default userRouter;
---
+++ src/routers/videoRouter.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const videoRouter = express.Router();
+
+videoRouter.get("/watch", handleWatch);
+videoRouter.get("/edit", handleEdit);
+export default videoRouter;
