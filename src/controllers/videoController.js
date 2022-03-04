6b98fa8bd10b64261b2bcd056f49a246204dd2b5
monkeyhurray <96379015+monkeyhurray@users.noreply.github.com>
2022-03-04 23:45:23 +0900

    study

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,24 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const search = (req, res) => res.send("Search");
+export const trending = (req, res) => {
+  res.send(
+    "<!DOYCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Home</h1><footer></footer>&copy; 2022Wetube - All rights reserved</body></html>"
+  );
+};
 
 export const see = (req, res) => {
-  console.log(req.params);
-  return res.send(`Watch Video ${req.params.id}`);
+  return res.send(
+    `<!DOYCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Watch video #${req.params.id}</h1><footer></footer>&copy; 2022Wetube - All rights reserved</body></html>`
+  );
 };
+
 export const edit = (req, res) => {
-  console.log(req.params);
-  return res.send("Edit");
+  return res.send(
+    `<!DOYCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Edit video #${req.params.id}</h1><footer></footer>&copy; 2022Wetube - All rights reserved</body></html>`
+  );
 };
+
+export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
+
 export const deleteVideo = (req, res) => {
   console.log(req.params);
   return res.send("DeleteVideo");
