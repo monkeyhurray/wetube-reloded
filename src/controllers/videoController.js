23b0935c542d8a8f931a0a961a6096d61e1c0ea8
monkeyhurray <96379015+monkeyhurray@users.noreply.github.com>
2022-03-11 22:49:23 +0900

    study

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,9 @@
-export const edit = (req, res) => res.render("edit");
-
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
-
+export const trending = (req, res) => {
+  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => res.render("watch", { pageTitle: "watch" });
-
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 
