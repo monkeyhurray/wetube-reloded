902c16e7c045e1bd7c415d8897475232711fe833
monkeyhurray <96379015+monkeyhurray@users.noreply.github.com>
2022-05-13 22:41:04 +0900

    study

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-import "./db";
-import "./models/Video";
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import req from "express/lib/request";
 
 const app = express();
 const logger = morgan("dev");
@@ -13,6 +13,25 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.get("/add-one", (req, res, next) => {
+  req.session.potato += 1;
+  return res.send(`${req.session.id}\n${req.session.potato}`);
+});
+
+app.use((req, res, next) => {
+  req.sessionStore.all((error, session) => {
+    console.log(session);
+    next();
+  });
+});
 //middleware는 위에 존재해야함
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
