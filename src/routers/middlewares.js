ab95b273622089c336770eb6af569e8fda8b7433
monkeyhurray <96379015+monkeyhurray@users.noreply.github.com>
2022-05-27 22:39:30 +0900

    study

--- src/routers/middlewares.js
+++ src/routers/middlewares.js
@@ -1,6 +1,23 @@
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
-  res.locals.loggedInUser = req.session.user;
+  res.locals.loggedInUser = req.session.user || {}; //user controller에 보낼 필요가 없음
   next();
+};
+//현재 로그인된 사용자를 가리킴
+
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
 };
